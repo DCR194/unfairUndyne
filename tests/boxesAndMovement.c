fc5118e05d4738ca4418330d58229aeeb156b6ca
Diego Ciudad Real <diegociudadreale@gmail.com>
2024-03-25 17:18:49 -0400

    Added draw functions to Boxes and movemente tests

--- tests/boxesAndMovement.c
+++ tests/boxesAndMovement.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <stdbool.h>
+#define SIZECUBES 5
+#define XWIDTH 320
+#define YHEIGHT 240
 
 
 struct Box {
@@ -10,30 +14,61 @@ struct Box {
     int direction;
 };
 
-
+// LOGIC FUNCTIONS
 void printBox(struct Box* box);
 void removeBox(struct Box** boxPtr, int index);
 void setBoxValues(struct Box* box, int direction);
 void updatePosition(struct Box* box);
 void addBox(struct Box** boxPtr, int direction);
 int checkHitbox(struct Box* boxPtr, int directionFacing);
+void swap(int* a, int* b);
+
+
+// HEX FUNCTIONS
 int getSWValue();
 int decodeHex(int value);
 void setSevenSegment(volatile long* segAddress, int value);
+
+// DRAWING FUNCTIONS
+void draw_line(int x0, int y0, int x1, int y1, short int line_color);
 void plot_pixel(int x, int y, short int line_color);
+void drawCube(struct Box a, struct Box b);
+void wait_for_vsync();
+void clear_screen();
+void draw_line(int x0, int y0, int x1, int y1, short int line_color);
+
 
 int numBoxes = 0;
 int globalTime = 0;
 
-int main(){
+short int Buffer1[YHEIGHT][XWIDTH + 200]; // 240 rows, 512 (320 + padding) columns
+short int Buffer2[YHEIGHT][XWIDTH + 200];
+
+volatile int* pixel_ctrl_ptr = (int*)0xFF203020;
+volatile int pixel_buffer_start;
+
+int main() {
     struct Box* boxPtr = NULL;
 
-    while(1){
+    clear_screen();
+    wait_for_vsync();
+
+    while (1) {
 
     }
 
 }
 
+
+
+void drawCube(struct Box a, struct Box b) {
+    for (int i = a.xPos - SIZECUBES / 2; i < a.xPos + SIZECUBES / 2; i++) {
+        for (int j = a.yPos - SIZECUBES / 2; j < a.yPos + SIZECUBES / 2; j++) {
+            plot_pixel(i, j, 0xFFFF);
+        }
+    }
+    draw_line(b.xPos, b.yPos, a.xPos, a.yPos, 0xFFFF);
+}
 
 void plot_pixel(int x, int y, short int line_color) {
     volatile short int* one_pixel_address;
@@ -41,8 +76,52 @@ void plot_pixel(int x, int y, short int line_color) {
     *one_pixel_address = line_color;
 }
 
-void drawBox(struct Box* myBox){
+// void drawBox(struct Box* myBox){
 
+//}
+
+void draw_line(int x0, int y0, int x1, int y1, short int line_color) {
+    bool is_steep = abs(y1 - y0) > abs(x1 - x0);
+    if (is_steep) {
+        swap(&x0, &y0);
+        swap(&x1, &y1);
+    }
+    if (x0 > x1) {
+        swap(&x0, &x1);
+        swap(&y0, &y1);
+    }
+    int deltax = x1 - x0;
+    int deltay = abs(y1 - y0);
+    int error = -(deltax / 2);
+    int y = y0;
+    int y_step;
+    if (y0 < y1) {
+        y_step = 1;
+    }
+    else {
+        y_step = -1;
+    }
+
+    for (int x = x0; x < x1; x++) {
+        if (is_steep) {
+            plot_pixel(y, x, line_color);
+        }
+        else {
+            plot_pixel(x, y, line_color);
+        }
+        error = error + deltay;
+        if (error > 0) {
+            y = y + y_step;
+            error = error - deltax;
+        }
+    }
+}
+
+void clear_screen() {
+    int y, x;
+    for (x = 0; x < 320; x++)
+        for (y = 0; y < 240; y++)
+            plot_pixel(x, y, 0);
 }
 
 void printBox(struct Box* box) {
@@ -174,4 +253,22 @@ void addBox(struct Box** boxPtr, int direction) {
 
     // Call setBoxValues to initialize the attributes of the newest box
     setBoxValues(&(*boxPtr)[numBoxes - 1], direction);
+}
+
+void wait_for_vsync() {
+    volatile int* pixel_ctrl_ptr = (int*)0xFF203020;
+    *pixel_ctrl_ptr = 1;
+    int status;
+    while (1) {
+        status = *(pixel_ctrl_ptr + 3);
+        if ((status & 1) == 0) {
+            return;
+        }
+    }
+}
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    (*a) = (*b);
+    (*b) = temp;
 }
