7f7bb9c9accc18393bfdd24a5003f76dbd81d549
E_N <98064547+Erfan-NA@users.noreply.github.com>
2024-04-05 04:59:35 -0400

    all the changes i DM'd about

--- src/BoxLogic.c
+++ src/BoxLogic.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include "boxLogic.h"
 
+int numBoxes = 0;
+int directionFacing = 0;
+struct Box* boxPtr = NULL;
 
-struct Box {
-    int xPos;
-    int yPos;
-    int xDir;
-    int yDir;
-    int direction;
-};
-
-
-void printBox(struct Box* box);
-void removeBox(struct Box** boxPtr, int* numBoxes, int index);
-void setBoxValues(struct Box* box, int direction);
-void updatePosition(struct Box* box);
-void addBox(struct Box** boxPtr, int* numBoxes, int direction);
-int checkHitbox(struct Box* boxPtr, int directionFacing);
-void updateAllBoxes(struct Box* boxPtr, int numBoxes);
 
 int main() {
-    int numBoxes = 0;
-    struct Box* boxPtr = NULL;
 
     // Example usage of addBox function to create 3 boxes
-    addBox(&boxPtr, &numBoxes, 0);
-    addBox(&boxPtr, &numBoxes, 1);
-    addBox(&boxPtr, &numBoxes, 2);
+    addBox(&boxPtr, 0);
+    addBox(&boxPtr, 1);
+    addBox(&boxPtr, 2);
 
     
 
@@ -54,11 +40,11 @@ int main() {
     printf("\nboxPtr[0] before moving to shield\n");
     printBox(&boxPtr[0]);
     int iterations = 0;
-    while (checkHitbox(&boxPtr[0], boxPtr[0].direction) == 0) { 
+    while (checkHitbox(&boxPtr[0]) == 0) { 
       updatePosition(&boxPtr[0]);
       iterations += 1;
     }
-    printf("\nboxPtr[0] after moving to shield %d after %d iterations\n", checkHitbox(&boxPtr[0], boxPtr[0].direction), iterations);
+    printf("\nboxPtr[0] after moving to shield %d after %d iterations\n", checkHitbox(&boxPtr[0]), iterations);
     printBox(&boxPtr[0]);
 
   
@@ -66,11 +52,11 @@ int main() {
     printf("\nboxPtr[1] before moving to shield\n");
     printBox(&boxPtr[1]);
     iterations = 0;
-    while (checkHitbox(&boxPtr[1], 0) == 0) { 
-      updateAllBoxes(boxPtr, numBoxes);
+    while (checkHitbox(&boxPtr[1]) == 0) { 
+      updateAllBoxes(boxPtr);
       iterations += 1;
     }
-    printf("\nboxPtr[1] after moving to shield %d after %d iterations\n", checkHitbox(&boxPtr[1], 0), iterations);
+    printf("\nboxPtr[1] after moving to shield %d after %d iterations\n", checkHitbox(&boxPtr[1]), iterations);
     printBox(&boxPtr[1]);
 
 
@@ -78,11 +64,11 @@ int main() {
     printf("\nboxPtr[2] before moving to shield\n");
     printBox(&boxPtr[2]);
     iterations = 0;
-    while (checkHitbox(&boxPtr[2], 0) == 0) {
+    while (checkHitbox(&boxPtr[2]) == 0) {
         updatePosition(&boxPtr[2]);
         iterations += 1;
     }
-    printf("\nboxPtr[2] after moving to shield %d after %d iterations\n", checkHitbox(&boxPtr[2], 0), iterations);
+    printf("\nboxPtr[2] after moving to shield %d after %d iterations\n", checkHitbox(&boxPtr[2]), iterations);
     printBox(&boxPtr[2]);
 
 
@@ -95,164 +81,19 @@ int main() {
     printBox(&boxPtr[0]);
     printBox(&boxPtr[1]);
     printBox(&boxPtr[2]);
-    removeBox(&boxPtr, &numBoxes, 1);
+    removeBox(&boxPtr, 1);
 
     printf("\nBoxes after removing box[1]:\n");
     printBox(&boxPtr[0]);
     printBox(&boxPtr[1]);
   
-    removeBox(&boxPtr, &numBoxes, 0);
+    removeBox(&boxPtr, 0);
     printf("Removed 0th box\n");
     printBox(&boxPtr[0]);
     
   
-    removeBox(&boxPtr, &numBoxes, 0);
+    removeBox(&boxPtr, 0);
     printf("Remove done\n");
 
     return 0;
-}
-
-
-
-void printBox(struct Box* box) {
-    printf("xPos: %d, yPos: %d, xDir: %d, yDir: %d\n", 
-    box->xPos, box->yPos, box->xDir, box->yDir);
-}
-
-// Function to remove the first box (FIFO)
-void removeBox(struct Box** boxPtr, int* numBoxes, int index) {
-    // Check if the index is valid
-    if (index < 0 || index >= *numBoxes) {
-        printf("Invalid index\n");
-        return;
-    }
-
-    // Shift the memory to the left to remove the box
-    for (int i = index; i < *numBoxes - 1; i++) {
-        (*boxPtr)[i] = (*boxPtr)[i + 1];
-    }
-
-    // Reallocate memory to decrease the size of the box array
-    *boxPtr = (struct Box*)realloc(*boxPtr, (*numBoxes - 1) * sizeof(struct Box));
-
-    // Decrement the number of boxes
-    (*numBoxes)--;
-}
-
-// Function to initialize the attributes of the newest box
-void setBoxValues(struct Box* box, int direction) {
-    // Set position and direction values based on the direction parameter
-    if (direction == 0) { //Laft
-        box->xPos = 0;
-        box->yPos = 119;
-        box->xDir = 1;
-        box->yDir = 0;
-        box->direction = 0;
-    }
-    if (direction == 1) { //Right
-        box->xPos = 319;
-        box->yPos = 119;
-        box->xDir = -1;
-        box->yDir = 0;
-        box->direction = 1;
-    }
-    if (direction == 2) { //Up
-        box->xPos = 159;
-        box->yPos = 0;
-        box->xDir = 0;
-        box->yDir = 1;
-        box->direction = 2;
-    }
-    if (direction == 3) { //Down
-        box->xPos = 159;
-        box->yPos = 239;
-        box->xDir = 0;
-        box->yDir = -1;
-        box->direction = 3;
-    }
-    // Add other conditions as needed
-}
-
-void updatePosition(struct Box* box) {
-    box->xPos += box->xDir;
-    box->yPos += box->yDir;
-}
-
-int checkHitbox(struct Box* boxPtr, int directionFacing) {
-    if (boxPtr->direction == 0 && 159 - boxPtr->xPos < 30) { //Left arrow hitboxes
-        if (159 - boxPtr->xPos < 9) {
-            return -2;
-        }
-        else if (directionFacing == 0) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    if (boxPtr->direction == 1 && boxPtr->xPos - 159 < 30) { //Right arrow hitboxes
-        if (boxPtr->xPos - 159 < 9) {
-            return -2;
-        }
-        else if (directionFacing == 1) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    if (boxPtr->direction == 2 && 119 - boxPtr->yPos < 30) { //Up arrow hitboxes
-        if (119 - boxPtr->yPos < 9) {
-            return -2;
-        }
-        else if (directionFacing == 2) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    if (boxPtr->direction == 3 && boxPtr->yPos - 119 < 30) { //Down arrow hitboxes
-        if (boxPtr->yPos - 119 < 9) {
-            return -2;
-        }
-        else if (directionFacing == 3) {
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    return 0;
-
-
-
-}
-
-// Function to add a box
-void addBox(struct Box** boxPtr, int* numBoxes, int direction) {
-    // Reallocate memory to increase the size of the box array
-    *boxPtr = (struct Box*)realloc(*boxPtr, (*numBoxes + 1) * sizeof(struct Box));
-
-    // Check if memory reallocation was successful
-    if (*boxPtr == NULL) {
-        printf("Memory reallocation failed\n");
-        return;
-    }
-
-    // Increment the number of boxes
-    (*numBoxes)++;
-
-    // Call setBoxValues to initialize the attributes of the newest box
-    setBoxValues(&(*boxPtr)[*numBoxes - 1], direction);
-}
-
-void updateAllBoxes(struct Box* boxPtr, int numBoxes) {
-    for (int i = 0; i < numBoxes; i++) {
-        updatePosition(&boxPtr[i]);
-    }
 }
--- src/userInput.c
+++ src/userInput.c
@@ -1,126 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
-void HEX_PS2(char, char, char);
 
-int main() {
-	volatile int * PS2_ptr = (int *)0xFF200100;
-	int PS2_data, RVALID, RAVAIL;
-	char key = 0;
-	char press, pressed;
-	int output;
-	while (1) {
-		PS2_data = *(PS2_ptr);
+int checkUserInput() {
+
+    volatile int * PS2_ptr = (int *)0xFF200100;
+    int PS2_data, RVALID, RAVAIL;
+    char key = 0;
+    char press, pressed;
+    int output;
+    PS2_data = *(PS2_ptr);
         RVALID = PS2_data & 0x8000; // extract the RVALID field
-        RAVAIL = PS2_data & 0xFFFF0000;
-		
-
+  
+  
         if (RVALID)
         {
             /* save the last three bytes of data */
-			
-			
             press = key;
             pressed = ((press&0xFF) == 0xF0)? 1 : 0;
             key = PS2_data & 0xFF;
-            
+  
             switch (key) {
-              case 0x6B: //left
-				if (pressed) {
-                	HEX_PS2(0, pressed, key);
-				}
-				else {
-					HEX_PS2(0, pressed, key);
-				}
-                break;
-              case 0x74: //right
-				if (pressed) {
-                	HEX_PS2(0, pressed, key);
-				}
-				else {
-					HEX_PS2(0, pressed, key);
-				}
-                break;
-              case 0x75: //up
-                if (pressed) {
-                	HEX_PS2(0, pressed, key);
-				}
-				else {
-					HEX_PS2(0, pressed, key);
-				}
-                break;
-              case 0x72: //down
-                if (pressed) {
-                	HEX_PS2(0, pressed, key);
-				}
-				else {
-					HEX_PS2(0, pressed, key);
-				}
-                break;
-
-              case 0x1C: //A key
-                if (pressed) {
-                	HEX_PS2(1, pressed, key);
-				}
-				else {
-					HEX_PS2(1, pressed, key);
-				}
-                break;
-              case 0x23: //D key
-                if (pressed) {
-                	HEX_PS2(1, pressed, key);
-				}
-				else {
-					HEX_PS2(1, pressed, key);
-				}
-                break;
-              case 0x1D: //W key
-                if (pressed) {
-                	HEX_PS2(1, pressed, key);
-				}
-				else {
-					HEX_PS2(1, pressed, key);
-				}
-                break;
-              case 0x1B: //S key
-				if (pressed) {
-                	HEX_PS2(1, pressed, key);
-				}
-				else {
-					HEX_PS2(1, pressed, key);
-				}
-                break;
+                case 0x6B: //left
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+                case 0x74: //right
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+                case 0x75: //up
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+                case 0x72: //down
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+        
+                case 0x1C: //A key
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+                case 0x23: //D key
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+                case 0x1D: //W key
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
+                case 0x1B: //S key
+                    return output = 0xFFFFFF & ((0x00 << 16) | (pressed << 8) | (key));
             }        
         }
-	}
-}
-
-
-
-
-/****************************************************************************************
-* Subroutine to show a string of HEX data on the HEX displays
-****************************************************************************************/
-void HEX_PS2(char b1, char b2, char b3) {
-	volatile int * HEX3_HEX0_ptr = (int *)0xFF200020;
-	volatile int * HEX5_HEX4_ptr = (int *)0xFF200030;
-	/* SEVEN_SEGMENT_DECODE_TABLE gives the on/off settings for all segments in
-	* a single 7-seg display in the DE1-SoC Computer, for the hex digits 0 - F
-	*/
-	unsigned char seven_seg_decode_table[] = {
-		0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
-		0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
-	unsigned char hex_segs[] = {0, 0, 0, 0, 0, 0, 0, 0};
-	unsigned int shift_buffer, nibble;
-	unsigned char code;
-	int i;
-	shift_buffer = (b1 << 16) | (b2 << 8) | b3;
-	for (i = 0; i < 6; ++i) {
-		nibble = shift_buffer & 0x0000000F; // character is in rightmost nibble
-		code = seven_seg_decode_table[nibble];
-		hex_segs[i] = code;
-		shift_buffer = shift_buffer >> 4;
-	}
-	/* drive the hex displays */
-	*(HEX3_HEX0_ptr) = *(int *)(hex_segs);
-	*(HEX5_HEX4_ptr) = *(int *)(hex_segs + 4);
+    return 0;
 }
